10a568bc0cb82881243bb86a260f2ff392d8642c
“gabriel6181997” <gabriel6181997@gmail.com>
2021-01-19 14:58:36 +0800

    Add hash to filename

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
   entry:'./src/javascripts/main.js',
   output: {
     path: path.resolve(__dirname,'./dist'),
-    filename: 'javascripts/main.js',
+    filename: 'javascripts/[name]-[hash].js',
   },
   module: {
     rules: [
@@ -59,7 +59,7 @@ module.exports = {
             loader: 'file-loader',
             options: {
               esModule: false,
-              name:'images/[name].[ext]',
+              name:'images/[name]-[hash].[ext]',
               publicPath: '/',
             },
           },
@@ -93,7 +93,7 @@ module.exports = {
   },
   plugins: [
     new MiniCssExtraPlugin({
-      filename:'./stylesheets/main.css',
+      filename:'./stylesheets/[name]-[hash].css',
     }),
     new HtmlWebpackPlugin({
       template: './src/templates/index.pug',
